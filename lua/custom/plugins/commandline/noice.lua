c787762ecc7293f0c5a5da252e9d5be7c141aeb9
johnnylarner <james.richardson.2556@gmail.com>
2025-02-04 09:24:39 +0100

    refactor: wrap notifications

--- lua/custom/plugins/commandline/noice.lua
+++ lua/custom/plugins/commandline/noice.lua
@@ -35,12 +35,13 @@ return -- lazy.nvim
       'rcarriga/nvim-notify',
       opts = {
         stages = 'fade_in_slide_out',
+        render = 'wrapped-default',
         top_down = true,
         max_height = function()
-          return math.floor(vim.o.lines * 0.75)
+          return math.floor(vim.o.lines * 0.95)
         end,
         max_width = function()
-          return math.floor(vim.o.columns * 0.75)
+          return math.floor(vim.o.columns * 0.25)
         end,
         timeout = 500,
       },
