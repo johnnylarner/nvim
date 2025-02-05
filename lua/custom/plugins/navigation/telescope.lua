f3482cce02b76042316388eccf0c70ea89e01f4d
johnnylarner <james.richardson.2556@gmail.com>
2025-02-05 12:05:08 +0100

    feat: add buffer wipe command

--- lua/custom/plugins/navigation/telescope.lua
+++ lua/custom/plugins/navigation/telescope.lua
@@ -115,6 +115,7 @@ return {
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>bw', ':bprevious | bdelete #<CR>', { desc = '[B]uffer [W]ipe' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
