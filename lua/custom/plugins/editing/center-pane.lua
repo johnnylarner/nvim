a54596e9d3ee3021e48648dba91ab731715906b0
johnnylarner <james.richardson.2556@gmail.com>
2026-01-23 19:40:41 +0100

    feat: add centring

---
+++ lua/custom/plugins/editing/center-pane.lua
@@ -0,0 +1,11 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  lazy = false,
+  opts = {
+    width = 148,
+    autocmds = {
+      enableOnVimEnter = true,
+      skipEnteringNoNeckPainBuffer = true,
+    },
+  },
+}
